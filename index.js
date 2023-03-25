a4f14d3e8b4bc4bb5168f0598efaa5c5fd623ccf
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-25 17:09:58 -0600

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your code here
+function submitData(myName, myEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: myName, email: myEmail})
+        })
+        .then(response => response.json())
+        .then(data => {document.body.textContent = data.id})
+        .catch(error => {document.body.textContent = error.message})
+    }
